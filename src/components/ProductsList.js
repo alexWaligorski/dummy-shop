06f5ff66eca138719546364803dfd815a93d7a7c
Alexandra Waligorski <waligorski.alexandra@gmail.com>
2023-06-05 13:03:22 +0200

    adjust Product List to show best Rated Products

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function ProductsList({ products }) {
+export default function ProductsList({ products, listType }) {
   return (
     <>
-      <h2>Products</h2>
+      {listType === "bestRated" ? (
+        <h2>Best Rated Products</h2>
+      ) : (
+        <h2>Products</h2>
+      )}
       <ul>
         {products.map((product) => (
           <li key={product.id}>
-            <Link to={`/products/${product.id}`}>{product.title}</Link>
+            <Link to={`/products/${product.id}`}>
+              {product.title}
+              {" // "} {product.rating}
+            </Link>
           </li>
         ))}
       </ul>
