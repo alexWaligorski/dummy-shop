b316f70ba0476bb9e0d3f4adcf228e80e330e6a4
Alexandra Waligorski <waligorski.alexandra@gmail.com>
2023-06-08 16:52:10 +0200

    add favoriteProductsSlice and implement Liketoggle

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../features/api/apiSlice";
 import cartReducer from "../features/cart/cartSlice";
+import favoriteProductsReducer from "../features/favoriteProducts/favoritePoductsSlice";
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
+    favoriteProducts: favoriteProductsReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
