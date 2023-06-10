ec331099d83ba25b5d551c402621b62545e8156e
Alexandra Waligorski <waligorski.alexandra@gmail.com>
2023-06-10 16:11:19 +0200

    add a reducer to cart to increase quantity of products by certain amount, implement in SingleProductPage

--- src/features/SingleProductPage.js
+++ src/features/SingleProductPage.js
@@ -1,14 +1,78 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useGetProductQuery } from "./api/apiSlice";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { productQuantitySetTo } from "./cart/cartSlice";
 
 export default function SingleProductPage() {
   const { productId } = useParams();
+  const dispatch = useDispatch();
   const { data: product, isLoading } = useGetProductQuery(productId);
+  const [amountOfItems, setAmountOfItems] = useState(1);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (product) {
+      const parsedProductId = parseInt(productId);
+      const productInCart = cart.find(
+        (cartItem) => cartItem.id === parsedProductId
+      );
+      if (productInCart) {
+        const productQuantity = productInCart.quantity;
+        setAmountOfItems(productQuantity);
+      }
+    }
+  }, [product, productId, cart]);
+
+  function onAddItemsToCart(event) {
+    event.preventDefault();
+    const updatedProduct = { ...product, quantity: amountOfItems };
+    dispatch(productQuantitySetTo(updatedProduct));
+  }
 
   if (isLoading) {
     return <h1>Is Loading...</h1>;
   }
 
-  return product && <h1>{product.title}</h1>;
+  return (
+    product && (
+      <article>
+        <h1>{product.title}</h1>
+        <img
+          src={product.thumbnail}
+          alt={product.title}
+          style={{ width: "20%", height: "auto" }}
+        />
+        <p>{product.description}</p>
+        <p>{product.price} €</p>
+        <form onSubmit={onAddItemsToCart}>
+          <button
+            disabled={amountOfItems === 0}
+            onClick={() =>
+              setAmountOfItems((prevAmountOfItems) => prevAmountOfItems - 1)
+            }
+            type="button"
+          >
+            -
+          </button>
+          <input
+            name="amount"
+            type="text"
+            value={amountOfItems}
+            onChange={(event) => setAmountOfItems(parseInt(event.target.value))}
+          />
+          <button
+            type="button"
+            onClick={() =>
+              setAmountOfItems((prevAmountOfItems) => prevAmountOfItems + 1)
+            }
+          >
+            +
+          </button>
+          <button>{product.price * amountOfItems} €</button>
+        </form>
+      </article>
+    )
+  );
 }
